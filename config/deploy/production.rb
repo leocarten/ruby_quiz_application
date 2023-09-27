4bae8d23a6681cd81eb0bb80a484414656b4e90e
Leo Carten <lcarten14@gmail.com>
2023-09-27 14:04:42 -0400

    new test -Leo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '159.89.37.13', user: 'deploy', roles: %w{app db web}
+server '167.71.105.117', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
